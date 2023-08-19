062de5482c2a6739e1449e5149b1c838bfec0861
sancoza-developer <140257603+sancoza-developer@users.noreply.github.com>
2023-08-19 17:36:19 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -103,3 +103,14 @@ $(function () {
   // animate on scroll
   new WOW().init();
 });
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
